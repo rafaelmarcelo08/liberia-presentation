41e681b6b8c7cef30bb6095d5d5b59828d91078b
Rafael Marcelo <rafael.2018.ads@gmail.com>
2025-05-25 18:13:45 -0300

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,ts}"],
+  theme: {
+    extend: {
+      animation: {
+        "fade-in": "fadeIn 1.5s ease-out forwards",
+        bounce: "bounce 1s infinite",
+      },
+      keyframes: {
+        fadeIn: {
+          "0%": { opacity: 0 },
+          "100%": { opacity: 1 },
+        },
+      },
+    },
+  },
+  plugins: [],
+};
