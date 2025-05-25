766bcab0b6b3d136e0bae309e1c3fd77e7645eb1
Rafael Marcelo <rafael.2018.ads@gmail.com>
2025-05-25 19:27:33 -0300

    ajustes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,15 +2,30 @@
 module.exports = {
   content: ["./src/**/*.{html,ts}"],
   theme: {
+    // tailwind.config.js
     extend: {
       animation: {
-        "fade-in": "fadeIn 1.5s ease-out forwards",
-        bounce: "bounce 1s infinite",
+        "fade-up": "fadeUp 1.2s ease-out forwards",
+        "fade-in": "fadeIn 1s ease-in forwards",
+        "slide-in-left": "slideInLeft 1s ease-out forwards",
+        "zoom-in": "zoomIn 1s ease-out forwards",
       },
       keyframes: {
+        fadeUp: {
+          "0%": { opacity: 0, transform: "translateY(30px)" },
+          "100%": { opacity: 1, transform: "translateY(0)" },
+        },
         fadeIn: {
           "0%": { opacity: 0 },
           "100%": { opacity: 1 },
+        },
+        slideInLeft: {
+          "0%": { opacity: 0, transform: "translateX(-40px)" },
+          "100%": { opacity: 1, transform: "translateX(0)" },
+        },
+        zoomIn: {
+          "0%": { opacity: 0, transform: "scale(0.95)" },
+          "100%": { opacity: 1, transform: "scale(1)" },
         },
       },
     },
